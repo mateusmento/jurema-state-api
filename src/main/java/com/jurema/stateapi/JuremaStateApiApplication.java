bb800ac816cca48527b926cdbbcde46fdc97e34c
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-03-19 13:42:06 -0300

    Initial commit

---
+++ src/main/java/com/jurema/stateapi/JuremaStateApiApplication.java
@@ -0,0 +1,13 @@
+package com.jurema.stateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuremaStateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuremaStateApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jurema/stateapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jurema.stateapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JuremaStateApiApplication.class);
+	}
+
+}
