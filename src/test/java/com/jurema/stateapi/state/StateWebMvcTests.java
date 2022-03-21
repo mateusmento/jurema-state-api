e67d917f959580d32c5b0b48ae60c3295a685c19
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-03-20 22:40:42 -0300

    Add StateWebMvcTests

--- src/test/java/com/jurema/stateapi/state/StateWebMvcTests.java
+++ src/test/java/com/jurema/stateapi/state/StateWebMvcTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,39 +13,32 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.List;
+
+import com.jurema.stateapi.statepopulation.StatePopulation;
+import com.jurema.stateapi.statepopulation.StatePopulationService;
+
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest
+@ComponentScan("com.jurema.stateapi.state")
 @AutoConfigureDataJpa
 public class StateWebMvcTests {
 
 	@Autowired
 	private MockMvc mvc;
 
-	@Test
-	void shouldRespondEmptyStateList() throws Exception {
-		// given
-		var request = get("/states");
-
-		// when
-		var response = mvc.perform(request);
-
-		// then
-		response.andExpectAll(
-			status().isOk(),
-			jsonPath("$", hasSize(0))
-		);
-	}
+	@MockBean
+	private StatePopulationService statePopulationService;
 
 	@Test
-	@Sql(statements = {
-		"insert into state (uf, name) values ('RJ', 'Rio de Janeiro')",
-		"insert into state (uf, name) values ('SP', 'São Paulo')"
-	})
+	@Sql("classpath:datasets/states.sql")
 	@Sql(statements = "delete from state", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	void shouldListStateFromDatabase() throws Exception {
 		// given
 		var request = get("/states");
+		when(statePopulationService.list()).thenReturn(statePopulations());
 
 		// when
 		var response = mvc.perform(request);
@@ -54,8 +49,25 @@ public class StateWebMvcTests {
 			jsonPath("$", hasSize(2)),
 			jsonPath("$[0].uf", equalTo("RJ")),
 			jsonPath("$[0].nome", equalTo("Rio de Janeiro")),
+			jsonPath("$[0].populacao", equalTo(1000)),
 			jsonPath("$[1].uf", equalTo("SP")),
-			jsonPath("$[1].nome", equalTo("São Paulo"))
+			jsonPath("$[1].nome", equalTo("São Paulo")),
+			jsonPath("$[1].populacao", equalTo(2000))
+		);
+	}
+
+	List<StatePopulation> statePopulations() {
+		return List.of(
+			new StatePopulation() {{
+				setId(1);
+				setUf("RJ");
+				setPopulation(1000);
+			}},
+			new StatePopulation() {{
+				setId(2);
+				setUf("SP");
+				setPopulation(2000);
+			}}
 		);
 	}
 }
